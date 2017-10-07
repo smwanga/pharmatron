4cda1f04c19251b4d7d90541a1f8f6df4b4c059c
Leitato Albert <wizqydy@gmail.com>
2017-10-07 15:07:50 +0300

    Add alternate button for invoice payment

--- resources/views/pos/sale-invoice.blade.php
+++ resources/views/pos/sale-invoice.blade.php
@@ -10,9 +10,14 @@
                   </a>
                   <a target="_blank" href="{{ route('sales.invoice.labels', $sale->id) }}" class="btn btn-white m-t-10 btn-block" type="button"><i class="fa fa-tag" data-toggle="tooltip" title="Print Lables"></i> Print Labels</a>
                   @can('accept_payments')
-                  @if($sale->due > 0)
+                  @if($sale->due > 0 && !$sale->dispensed_at)
                   <button data-url="{{ route('sales.invoice.pay', $sale->id) }}" data-toggle="tooltip" title="Accept Payment" class="btn btn-success ajaxModal btn-block m-t-10" type="button"><i class="fa fa-credit-card"></i>
                     @lang('main.pay_dispense')
+                  </button>
+                  @endif
+                  @if($sale->due > 0 && $sale->dispensed_at)
+                  <button data-url="{{ route('sales.invoice.pay', $sale->id) }}" data-toggle="tooltip" title="Accept Payment" class="btn btn-success ajaxModal btn-block m-t-10" type="button"><i class="fa fa-credit-card"></i>
+                    @lang('main.pay_cash')
                   </button>
                   @endif
                   @endcan
